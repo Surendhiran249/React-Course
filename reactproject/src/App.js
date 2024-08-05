dccaa0626e43241221907697fcaf8d9e407e0281
Surendhiran <727723eucs249@skcet.ac.in>
2024-08-05 15:59:09 +0530

    First commit

---
+++ reactproject/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import ff from './components/welcome';
+import ff from './components/welcome';
+
+function App(){
+  return(
+    <div>
+      <ff />
+    </div>
+  )
+}
+export default App;
